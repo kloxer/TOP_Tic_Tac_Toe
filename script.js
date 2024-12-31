640c05f5b8e7c589fe8d446b15c027d2b9daa24c
kloxer <102638907+kloxer@users.noreply.github.com>
2024-12-31 16:59:25 -0500

    Did all the styling

--- script.js
+++ script.js
@@ -77,17 +77,21 @@ function playTicTacToe(){
     let moves = 0; //Will be used to check for a tie 
     const players=[
         {
-            name: "Jon",
+            name: "Player1",
             token: "X",
             wins: 0
         },
         {
-            name: "Cece",
+            name: "Player2",
             token: "O",
             wins:0
         }
     ];
     let activePlayer = players[0];
+    
+    //Create an empty grid visually
+    const display = displayBoard();
+    display.createGrid();
 
     //Switch current player
     const switchPlayerTurn = () => {
@@ -101,39 +105,8 @@ function playTicTacToe(){
     };
     
     const gameTied = () => {
-        alert("we reached a tie, no winner");
         board.newBoard();
         moves=0; //Reset moves for the new board     
-    }
-
-    const playRound = function(){
-        //Active player should decide what position to put it in
-        //Token already decided for active player,
-        //When finished, active player should switch
-        let positionFromActive = prompt(activePlayer.name + " Enter a valid position(0,0 to 2,2)");
-        console.log(positionFromActive);
-        while (board.checkValidPosition(positionFromActive) == -1) {
-            console.log("not a valid position");
-            positionFromActive = prompt("That was not a valid position, enter a valid position(0,0 to 2,2)");
-        }
-        board.makeChoice(positionFromActive, activePlayer.token);
-        moves++;// increase 1 for each move made
-        if (checkWinConditon() == true){
-            alert("we have a winner");
-            board.newBoard(); //Reset the board
-            activePlayer.wins = activePlayer.wins + 1
-            console.log(activePlayer.name + " wins: " + activePlayer.wins);
-        }
-        //Check if there is a tie if 9 moves have been made
-        else if (moves == 9){
-            gameTied();
-        }
-
-        switchPlayerTurn(); //Switch the player
-        printNewRound(); //Print the board
-
-        
-
     }
 
     //Need to check the winconditoon
@@ -144,7 +117,7 @@ function playTicTacToe(){
         let diagonal = ""
         let vertical = ""
         let winCondition1 = "XXX";
-        let winCondition2 = "YYY";
+        let winCondition2 = "OOO";
         //Check horizontal and vertical win condition
         for (let i = 0; i < 3; i++) {
             horizontal = "";
@@ -190,8 +163,80 @@ function playTicTacToe(){
         return false
 
     };
+    const setFlag = (val) => flag=val;
+    let flag = false;
+    const playRound = function(){
+        if (flag == true){
+            return
+        }
+        currCell = display.getCurrentCell();
+        console.log(currCell);
+        //Active player should decide what position to put it in
+        //Token already decided for active player,
+        //When finished, active player should switch
+        let positionFromActive = currCell;
+        console.log(positionFromActive);
+        if (board.checkValidPosition(currCell) == -1){ //Check if valid position before doing anything else
+            return;
+        }
 
-    return {playRound, getActivePlayer, checkWinConditon};
+        display.placeMarker(currCell, activePlayer.token); //Put a marker on display
+ 
+
+        board.makeChoice(positionFromActive, activePlayer.token);
+        moves++;// increase 1 for each move made
+        if (checkWinConditon() == true){
+            display.displayMessage(activePlayer.name + " is the winner!");
+            flag= true;
+           
+            activePlayer.wins = activePlayer.wins + 1
+            display.setScore(players[0].wins,players[1].wins);
+
+            console.log(activePlayer.name + " wins: " + activePlayer.wins);
+
+            // board.newBoard(); //Reset the board
+            // display.resetGrid(); //Reset the grid
+            // moves=0; //reset moves to 0
+        }
+        //Check if there is a tie if 9 moves have been made
+        else if (moves == 9){
+            flag= true;
+            display.displayMessage("Game tied, no winner");
+            // gameTied();
+            // display.resetGrid();
+
+        }
+
+        switchPlayerTurn(); //Switch the player
+        printNewRound(); //Print the board
+
+        
+
+    };
+
+    const resetGame = function(){
+        display.displayMessage("");
+        moves= 0;
+        board.newBoard(); //Reset the board
+        display.resetGrid(); //Reset the grid
+        flag=false;
+    }
+
+    const setNames = function(name1, name2){
+
+        players[0].name = name1;
+        players[1].name = name2;
+        display.setDisplayNames(name1,name2); //Call the displayboard to visualize the names
+    };
+
+    const getNames = () => {
+        return [
+            players[0].name,
+            players[1].name
+        ];
+    };
+
+    return {playRound, getActivePlayer, checkWinConditon, setFlag, resetGame ,setNames, getNames};
 
 
 
@@ -203,7 +248,112 @@ function playTicTacToe(){
 // console.log(x.getGamePieceF());
 // console.log(r.makeChoice("0,0","X"));
 // r.printBoard();
+function displayBoard(){
+
+    const gameDiv= document.querySelector(".game");
+    let currentCell = "";
+
+    const getCurrentCell = () => currentCell;
+
+    const createGrid = function(){
+        for (let i = 0; i < 3; i++) {
+            for (let j = 0; j < 3; j++) {
+                const cell = document.createElement("button");
+                cell.setAttribute("class","cell");
+                cell.setAttribute("id", i + "," + j);   
+                cell.addEventListener("click", function(){
+                    currentCell =  i + "," + j;
+                    y.playRound();  //On a click, play a round
+
+                });
+                gameDiv.appendChild(cell); 
+
+            }
+        }
+
+    };
+
+    const placeMarker =function(position, marker){
+        const cell = document.getElementById(position);
+        cell.textContent = marker;
+    };
+
+    const resetGrid = function(){
+
+        const cell_array = document.querySelectorAll(".cell");
+        cell_array.forEach(cell => {
+            cell.textContent = "";
+        });
+                
+    };
+
+    const setDisplayNames = function(name1,name2){
+        const displayName1 = document.querySelector("#user1");
+        displayName1.textContent = name1;
+        const displayName2 = document.querySelector("#user2");
+        displayName2.textContent = name2;
+
+    }
+
+    const setScore = function(score1, score2){
+        const score1div = document.querySelector(".score1");
+        const score2div = document.querySelector(".score2");
+        score1div.textContent = score1;
+        score2div.textContent = score2;
+    }
+
+
+    const displayMessage = function(msg){
+        const text = document.querySelector(".message");
+        text.textContent = msg;
+    };
+    
+    return {createGrid, placeMarker, getCurrentCell, resetGrid, displayMessage, setDisplayNames, setScore};
+}   
+
 
 y = playTicTacToe();
-y.playRound();
-console.log(y.checkWinConditon());
+
+const resetButton = document.querySelector("#reset");
+resetButton.addEventListener("click", function(){
+    y.resetGame();
+    console.log("hi");
+});
+
+
+const updateButton = document.getElementById("updateDetails");
+const cancelButton = document.getElementById("cancel");
+const dialog = document.getElementById("favDialog");
+
+const firstname = favDialog.querySelector("#firstName");
+const secondname = favDialog.querySelector("#secondName");
+
+const confirmBtn = favDialog.querySelector("#confirmBtn");
+
+function openCheck(dialog) {
+  if (dialog.open) {
+    console.log("Dialog open");
+  } else {
+    console.log("Dialog closed");
+  }
+}
+
+// Update button opens a modal dialog
+updateButton.addEventListener("click", () => {
+    
+  dialog.showModal();
+  openCheck(dialog);
+});
+
+// Form cancel button closes the dialog box
+cancelButton.addEventListener("click", () => {
+  dialog.close("animalNotChosen");
+  openCheck(dialog);
+});
+
+// Prevent the "confirm" button from the default behavior of submitting the form, and close the dialog with the `close()` method, which triggers the "close" event.
+confirmBtn.addEventListener("click", (event) => {
+    event.preventDefault(); // We don't want to submit this fake form
+    y.setNames(firstname.value,secondname.value);
+    dialog.close();
+  });
